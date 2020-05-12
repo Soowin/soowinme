e9d214b827d6ff6108adfb90ddd0c2904d2bbdc4
soowin <soowin@live.com>
2020-05-12 14:00:33 +0800

    优化菜单

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,17 @@ module.exports = {
       { text: 'XDUNA', link: '/xduna/' },
       { text: 'Blogs', link: '/blogs/' },
       { text: 'Tools', link: '/tools/' },
-    ]
+    ],
+    logo: './head.png',
+    sidebar:{
+      '/tools/': [{
+        title:"Tools",
+        collapsable: false,
+        children:[
+          {title:"作业统计", path:"/tools/zuoyetongji"}
+        ]
+      }],
+      
+    }
   }
 }
